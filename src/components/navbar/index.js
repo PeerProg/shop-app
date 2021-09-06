6dcf3436b7863ec1c7772c1c6fc9f779ddc19da2
oreoluwade <oreoluwade@gmail.com>
2021-09-06 17:03:41 +0100

    Create basic Navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './Navbar.module.scss';
 
 const Navbar = () => {
-  return <div>This is the navbar</div>;
+  return (
+    <header className={styles.navbar}>
+      <Link href="/">
+        <a className={styles.navbar__item}>Home</a>
+      </Link>
+      <Link href="/checkout">
+        <a className={styles.navbar__item}>Checkout</a>
+      </Link>
+      <Link href="/cart">
+        <a className={styles.navbar__item}>Cart</a>
+      </Link>
+    </header>
+  );
 };
 
 export default Navbar;
