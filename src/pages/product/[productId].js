b5c16ab914f75532243332b08f212560ec3efd57
oreoluwade <oreoluwade@gmail.com>
2021-09-07 11:11:27 +0100

    Create product page

--- src/pages/product/[productId].js
+++ src/pages/product/[productId].js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import axios from 'axios';
+
 import styles from './Product.module.scss';
 
 const Product = () => {
   const router = useRouter();
+  const [productDetails, setProductDetails] = useState(null);
 
   const { productId } = router.query;
 
-  console.log('ProductId', productId);
+  console.log('Product Details', productDetails);
+
+  useEffect(() => {
+    if (productId) {
+      axios.get(`/api/products/${productId}`).then(res => {
+        setProductDetails(res.data);
+      });
+    }
+  }, [productId]);
 
   return <div>Product page</div>;
 };
