884286c0c94c28859092f037b36b6ca05fc6e91d
Oluwaseyi Aromokeye <aromokeyes2@gmail.com>
2021-09-28 11:48:14 -0400

    instagram login

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import styles from './Cart.module.scss';
+import React, { Fragment } from 'react';
+// import FacebookLogin from 'react-facebook-login';
+import InstagramLogin from 'react-instagram-login';
+// import styles from './Cart.module.scss';
+
+const ComponentClicked = (response) => (<div>Cart page</div>);
 
 const CartPage = () => {
-  return <div>Cart page</div>;
+  
+  const ResponseInstagram = (response) => {
+    console.log('Na error');
+    console.log(response);
+  }
+  
+    return (<Fragment>
+
+      {/* <FacebookLogin
+        appId="1546779488994882"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={ComponentClicked}
+        callback={responseFacebook}
+        /> */}
+        <InstagramLogin
+          clientId=""
+          buttonText="Login"
+          onSuccess={ComponentClicked}
+          onFailure={() => console.log('error')}
+  />
+    </Fragment>
+    );
 };
 
 export default CartPage;
